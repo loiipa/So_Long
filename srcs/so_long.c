762178141c30e43782e25802af51eaa02eb0747d
Jang Chang Hyun <cjang@student.42seoul.kr>
2021-11-24 13:37:15 +0900

    확장자명이 .ber일 때 대응

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 14:15:33 by cjang             #+#    #+#             */
-/*   Updated: 2021/11/21 21:17:05 by cjang            ###   ########.fr       */
+/*   Updated: 2021/11/24 13:36:15 by cjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 static int	check_open(int argv, char **argc)
 {
 	int		fd;
+	int		s_len;
+	int		cmp;
+	char	*extension_name;
 
+	extension_name = ".ber";
 	if (argv != 2)
-		error_user("Please write map location");
+		error_user("Please write map location\n");
+	s_len = ft_strlen(argc[1]);
+	if (s_len < 4)
+		error_user("please write correct map location\n");
+	cmp = ft_strncmp(&argc[1][s_len - 4], extension_name, 4);
+	if (cmp != 0)
+		error_user("Map filename extension must have .ber\n");
 	fd = open(argc[1], O_RDONLY);
 	if (fd < 0)
 		error_system();
